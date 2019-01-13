b9c99fb533e8caf71763ebd398bf50e678c5ba49
jodyneckles <jody@technative.tv>
2019-01-13 11:35:56 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,51 @@ const questions = [
 ]
 
 let question;
+let nextQuestion = questions[Math.floor(Math.random()*questions.length)];
+const questionContainer = document.querySelector('.question-container');
+
+function appendQuestion(question) {
+  return questionContainer.innerHTML = question.questionText;
+}
+
+function askQuestionThen(timeMs) {
+  question = undefined;
+  question = questions[0]
+  appendQuestion(question);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(question);
+    },timeMs)
+  })
+}
+
+function removeQuestion() {
+  return questionContainer.innerHTML = '';
+}
+
+function askQuestionThenRemoveQuestion(timeMs) {
+  return askQuestionThen(timeMs).then(removeQuestion);
+}
+
+function trueAndFalseButtons() {
+  return document.querySelectorAll(".btn.center-align");
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(button => button.classList.toggle('hide'));
+    // if (button.classList.contains('hide')) {
+    //   button.classList.remove('hide');
+    // } else {
+    //   button.classList.add('hide');
+    // }
+}
+
+function both() {
+  toggleTrueAndFalseButtons();
+  askQuestionThenRemoveQuestion(5000);
+}
+
+function displayQuestionOnClick() {
+  let askBtn = document.querySelector('a.btn');
+  askBtn.addEventListener('click', both);
+}
